85a47f33314dd899a6ead06950789c9fadf9fbdc
tomy21 <tomy21.agung@gmail.com>
2025-01-30 16:04:16 +0700

    update_

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://dashboard-on.skyparking.online"],
   })
 );
 
